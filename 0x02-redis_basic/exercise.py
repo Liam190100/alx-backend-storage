92f94be276ba42037e7a3c93a79f294c9a2ed7e3
root <jnnjuguna8@.com>
2024-03-27 12:27:07 -0700

    Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''Module request caching and tracking.
+'''
+from functools import wraps
+from typing import Callable
+import redis
+import requests
+
+
+redis_store = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''URL after caching the request's response,
+    then tracking the request.
+    '''
+    return requests.get(url).text
